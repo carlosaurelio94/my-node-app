8226b16e4643b8b04852bf9ff3e3f0d9b9c555ab
Carlos Rodriguez <carlosarc10@gmail.com>
2024-10-22 20:24:18 -0300

    HOST

--- functions/sendMail.js
+++ functions/sendMail.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
 });
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
